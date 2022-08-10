9d9397dc70d23c255b8ab596bcec389ae87f9b4c
Mark Yip <yip279@gmail.com>
2022-08-10 14:39:23 +0800

    Save the core logic  inside the codebase without touching other part.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // representing a journey by at each stop how many people get off the vehicle and what was the accumulated fare.
+
+  const [getOffs, setGetOffs] = useState(); // {accumulatedFare, numberOfPassengers}
+
+  const fairFares = getOffs.reduce((fees, currentGetOff, currentIndex) => {
+    const previousAccumulatedFare =
+      getOffs[currentIndex - 1]?.accumulatedFare ?? 0;
+
+    const marginalFare =
+      currentGetOff.accumulatedFare - previousAccumulatedFare;
+
+    const numberOfPassengersOnCar = getOffs
+      .map((getOff) => getOff.numberOfPassengers)
+      .slice(currentIndex)
+      .reduce((previousValue, currentValue) => previousValue + currentValue);
+
+    const marginalFairFare = marginalFare / numberOfPassengersOnCar;
+
+    fees.push((fees[currentIndex - 1] ?? 0) + marginalFairFare);
+    return fees;
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
