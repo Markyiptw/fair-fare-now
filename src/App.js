2cf8a140bdf07a8e490cf1b343f2d006c70b8d56
Mark Yip <yip279@gmail.com>
2022-08-11 11:49:22 +0800

    Remove unused styles and tests.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
 
 function App() {
   // representing a journey by at each stop how many people get off the vehicle and what was the accumulated fare.
 
-  const [getOffs, setGetOffs] = useState(); // {accumulatedFare, numberOfPassengers}
+  const [getOffs, setGetOffs] = useState([]); // {accumulatedFare, numberOfPassengers}
 
   const fairFares = getOffs.reduce((fees, currentGetOff, currentIndex) => {
     const previousAccumulatedFare =
@@ -24,24 +23,7 @@ function App() {
     return fees;
   }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
